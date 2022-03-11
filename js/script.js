5b3d3e2f70a06b10da5699f09c2f90ea5e24917d
Sachin Jadhav <jadhav.sachin1993@gmail.com>
2022-03-11 16:36:46 +0530

    Completed

--- js/script.js
+++ js/script.js
@@ -1,20 +1,7 @@
-function refreshPage(){
-    window.location.reload();
-} 
-
 const toggler = document.querySelector("#switch");
 const annual = document.querySelectorAll(".annual");
 const monthly = document.querySelectorAll(".monthly");
-window.onload = function priceChange(){
-    if (toggler.checked === false) {
-        monthly.forEach(hideMonthly);
-        function hideMonthly(item) {
-            item.style.display = "none";
-        }
-    } else if (toggler.checked === true ) {
-        annual.forEach(hideAnnual);
-        function hideAnnual(item) {
-            item.style.display = "none";
-        }
-    }
-}
+const container = document.querySelector(".container");
+toggler.addEventListener("click", () => {
+    container.classList.toggle("monthly", toggler.checked);
+});
